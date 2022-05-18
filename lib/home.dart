04af9e567632436607690bb3155b45697673cd1b
TresorKL <trezmathieu@gmail.com>
2022-05-18 17:19:43 +0200

    custom detection

--- lib/home.dart
+++ lib/home.dart
@@ -2,6 +2,7 @@ import 'package:camera/camera.dart';
 import 'package:face_detection/utils_scanner.dart';
 import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -40,13 +41,102 @@ class _HomeState extends State<Home> {
         if(!mounted){
           return;
         }
-        cameraController!.startImageStream((imageFromStream) => null)
-    })
+        cameraController!.startImageStream((imageFromStream) => {
+          if(!isWorking){
+             isWorking =true,
+          }
+        });
+    });
 
+  }
+  dynamic scanResults;
+
+  performDetectionOnStreamFrames(CameraImage cameraImage )async{
+    UtilsScanner.detect(image: cameraImage, detectImage: faceDetector!.processImage, imageRotation: description!.sensorOrientation
+    ).then((dynamic results){
+      setState(() {
+        scanResults = results;
+      });
+    }).whenComplete((){
+      isWorking=false;
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    initCamera();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    cameraController?.dispose();
+    faceDetector?.close();
+  }
+
+ Widget buildResult(){
+
+    if(scanResults== null || cameraController ==null || !cameraController!.value.isInitialized){
+      return const Text("");
+    }
+    final Size imageSize = Size(cameraController!.value.previewSize.height,cameraController!.value.previewSize.width);
+    CustomPainter customPainter = FaceDetectorPainter(imageSize, scanResults, cameraDirection);
+
+    return CustomPaint(painter: customPainter,);
   }
 
   @override
   Widget build(BuildContext context) {
     return Container();
+  }
+
+
+
+}
+
+class FaceDetectorPainter extends CustomPainter{
+
+  FaceDetectorPainter(this.absoluteImageSize, this.faces,this.cameraLensDirection);
+
+
+  final Size absoluteImageSize;
+  final List<Face>faces;
+  CameraLensDirection cameraLensDirection;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    // TODO: implement paint
+    final double scaleX = size.width/absoluteImageSize.width;
+    final double scaleY = size.height/absoluteImageSize.height;
+
+    final Paint paint = Paint()
+    ..style=PaintingStyle.stroke
+    ..strokeWidth=2.0
+    ..color=Colors.red;
+
+    for(Face face in faces) {
+      canvas.drawRect(
+          Rect.fromLTRB(
+              cameraLensDirection == CameraLensDirection.front
+                  ? (absoluteImageSize.width - face.boundingBox.right) * scaleX
+                  : face.boundingBox.left * scaleX,
+              face.boundingBox.top * scaleY,
+              cameraLensDirection == CameraLensDirection.front
+                  ? (absoluteImageSize.width - face.boundingBox.left) * scaleX
+                  : face.boundingBox.right * scaleX,
+              face.boundingBox.bottom * scaleY
+          ), paint
+      );
+    }
+  }
+
+  @override
+  bool shouldRepaint(FaceDetectorPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+
+     return oldDelegate.absoluteImageSize != absoluteImageSize || oldDelegate.faces != faces;
   }
 }
