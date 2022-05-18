339e136dd6626aaaeeb0ee7347ade61dc9fb2f37
TresorKL <trezmathieu@gmail.com>
2022-05-18 14:37:07 +0200

    set firabase files

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 import 'home.dart';
-
-void main() {
+List<CameraDescription>cameras;
+Future<void> main () async{
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
   runApp(MyApp());
 }
 
