44dd0d6902584ca19bed9aacfddcea780484e6ee
TresorKL <trezmathieu@gmail.com>
2022-05-18 14:45:31 +0200

    ML kit face detection

--- lib/home.dart
+++ lib/home.dart
@@ -1,12 +1,30 @@
+import 'package:camera/camera.dart';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 import 'package:flutter/cupertino.dart';
 class Home extends StatefulWidget {
-  const Home({Key key}) : super(key: key);
+  const Home({Key? key}) : super(key: key);
 
   @override
   State<Home> createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+
+  bool isWorking=false;
+
+  CameraController? cameraController;
+
+  FaceDetector? faceDetector;
+  Size? size;
+
+  List<Face>? faceList;
+
+  CameraDescription? description;
+
+  CameraLensDirection? cameraDirection;
+
+
+
   @override
   Widget build(BuildContext context) {
     return Container();
